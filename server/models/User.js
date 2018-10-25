77f146af27722b7c9524a01a7a98c3f61413680c
Ojuna Faust <ojuna.faust@gmail.com>
2018-10-25 19:20:24 +0200

    change user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,19 +1,31 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const userSchema = new Schema(
     {
         username: String,
         firstname: String,
         surname: String,
-        password: String,
-        email: String,
+        password: {
+            type: String,
+            required: true,
+            minlength: 5,
+            maxlength: 1024
+        },
+        email: {
+            type: String,
+            required: true,
+            minlength: 2,
+            maxlength: 50
+        },
         pictureUrl: String,
         street: String,
         number: String,
         postalCode: String,
         city: String,
-        district: String
+        district: String,
+        isAdmin: Boolean
     },
     {
         timestamps: {
@@ -22,6 +34,18 @@ const userSchema = new Schema(
         }
     }
 );
+userSchema.methods.generateAuthToken = function() {
+    const token = jwt.sign(
+        {
+            _id: this._id,
+            username: this.username,
+            email: this.email,
+            isAdmin: this.isAdmin
+        },
+        config.get('jwtPrivateKey')
+    );
+    return token;
+};
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
