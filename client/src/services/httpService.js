73af42f202f4ab863374731492da6c2eaa842867
Ojuna Faust <ojuna.faust@gmail.com>
2018-10-26 14:05:47 +0200

    restructure api.js in serveral seperate files

--- client/src/services/httpService.js
+++ client/src/services/httpService.js
@@ -10,12 +10,15 @@ axios.interceptors.response.use(null, error => {
 
     return Promise.reject(error);
 });
+
 function setJwt(jwt) {
     axios.defaults.headers.common['x-auth-token'] = jwt;
 }
+
 function setHeader(name, value) {
     axios.defaults.headers.common[name] = value;
 }
+
 export default {
     get: axios.get,
     post: axios.post,
